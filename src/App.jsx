c25cc3f12457cc4327d03caaaf63fa5c7dec3e1a
JVlCTOR <joaovictorresende59@gmail.com>
2024-01-16 16:25:47 -0300

    atualizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 
 import { Outlet } from 'react-router-dom';
-
 import { NavBar } from './components/navbar/NavBar';
 
-function App() {
-
+export default function App() {
 
   return (
-    <div className='w-full h-dvh m-0 p-0 bg-slate-800'>
+    <div className='w-screen h-screen bg-slate-800 md:bg-slate-800 sm:slate-800 overflow-y-scroll '>
         <NavBar/>
-      <div className='w-full h-vh text-white m-5 px-5'>
+      <div className='w-full h-full sm:bg-slate-800 md:bg-slate-800 text-white m-5 px-5'>
         <Outlet/>
       </div>
     </div>
   )
 }
 
-export default App
+ 
