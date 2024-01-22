8b73a190264dd17a13dd843be69021d6899e2c4f
raimundoneto01 <raimundogomes826@gmail.com>
2024-01-22 11:13:26 -0300

    subindo atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-
-import { Outlet } from 'react-router-dom';
-import { NavBar } from './components/navbar/NavBar';
+import { Outlet } from "react-router-dom";
+import { NavBar } from "./components/navbar/NavBar";
 
 export default function App() {
-
   return (
-    <div className='w-full h-screen bg-slate-900 overflow-y-scroll  '>
-        <NavBar/>
-      <div className='w-full h-full sm:bg-slate-900 md:bg-slate-900 text-white m-5 px-5'>
-        <Outlet/>
+    <div className="w-full h-screen bg-slate-900 overflow-y-scroll  ">
+      <NavBar />
+      <div className=" sm:bg-slate-900 md:bg-slate-900 text-white px-5">
+        <Outlet />
       </div>
     </div>
-  )
+  );
 }
-
- 
