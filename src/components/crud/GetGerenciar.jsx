648c67bc2d3baae986fdd6664cd3cbdf45cef09e
raimundoneto01 <raimundogomes826@gmail.com>
2024-01-22 12:07:09 -0300

    atualizando project de tela

--- src/components/crud/GetGerenciar.jsx
+++ src/components/crud/GetGerenciar.jsx
@@ -2,7 +2,7 @@ export function GetGerenciar(){
   return(
     <>
       
-      <p className="flex text-lg text-green-300 md:text-xl sm:text-2xl ">Sejam Bem Vindos, Espero que gostem do meu Projeto.</p>
+      <p className="flex text-lg text-green-300 md:text-2xl sm:text-2xl ">Sejam Bem Vindos, Espero que gostem do meu Projeto.</p>
       
     </>
   )
